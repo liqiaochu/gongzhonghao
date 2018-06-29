e0e6612fd69545bd7339a1d1944a3cbb90cfb7e1
liqiaochu <gmxysz@gmail.com>
2018-06-30 03:15:08 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+config = {
+    blog_name: "宋泽工作室",
+    github_username: "liqiaochu", // github 账号名
+    github_repo: "songze",      // github 建立的 repo 名
+    per_page: 10,             // 首页每次载入文章列表数量
+
+    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // 将配置好的 access token 拆分成两个字符串相加
+    access_token: "381ef83efcfce0c2db7ce"+"1296f66c30c92330186"
+};
