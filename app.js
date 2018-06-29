d43e19ae49d2b46c05862c9cff518323e236183e
liqiaochu <gmxysz@gmail.com>
2018-06-30 02:55:16 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,77 +1,126 @@
-function gotoHome()
-{
-    setTitle(config.blog_name);
-    setBlogName();
-    setFooter();
-    var page = 1;
-    var link = {};
-    var postlist = document.getElementById("post_list");
-    function loadPage(page) {
-        var more = document.getElementById("more");
-        if (more) {
-            removeChildById(postlist, "more");
-            var newMore = document.createElement("img");
-            newMore.id = "more";
-            newMore.src = "littlewait.gif";
-            postlist.appendChild(newMore);
-        }
-        getJSON(getPageUrl(page), function(data, headers) {
-            removeChildById(document.getElementById("index"), "wait");
-            removeChildById(postlist, "more");
-            for (var i=0; i<data.length; i++) {
-                var posttitle = document.createElement("li");
-                posttitle.className = "pagelist";
-                postlist.appendChild(posttitle);
-                var href = document.createElement("a");
-                href.href = "?p="+data[i].number+"&t="+(new Date().getTime());
-                var txt = document.createTextNode(data[i].title);
-                href.appendChild(txt);
-                posttitle.appendChild(href);
-            }
-
-            if (headers.Link) {
-                var linkArray = headers.Link.split(",");
-                for (var i=0; i<linkArray.length; i++) {
-                    var m = linkArray[i].match(/\?per_page=(\d+)&page=(\d+)>; rel="(\w+)"/);
-                    if (m) {
-                        link[m[3]] = {
-                            "per_page": m[1],
-                            "page": m[2]
-                        }
-                    }
-                }
-                var last = parseInt(link.last.page);
-                if (page<last) {
-                    var href = document.createElement("a");
-                    href.id = "more";
-                    href.href = "javascript:loadPage("+(page+1)+");";
-                    var txt = document.createTextNode("More");
-                    href.appendChild(txt);
-                    postlist.appendChild(href);
-                }
-            }
-        });
+Ractive.DEBUG = false;
+function index(page){
+    var page = parseInt(page) || 1;
+    window._G = window._G || {post: {}, postList: {}};
+    $('title').html(_config['blog_name']);
+    if(_G.postList[page] != undefined){
+      $('#container').html(_G.postList[page]);
+      return;
     }
-    loadPage(1);
-}
 
-function gotoPage(id)
-{
-    setTitle(config.blog_name);
-    setFooter();
-    getJSON(getIssuesUrl(id), function(data) {
-        setTitle(config.blog_name + " - " + data.title);
-        var title = document.getElementById("title");
-        var txt = document.createTextNode(data.title);
-        title.appendChild(txt);
-        var content = document.getElementById("content");
-        renderMarkdown(content, data.body);
-        var comment = document.getElementById("comment");
-        var href = document.createElement("a");
-        href.href = getCommentUrl(id);
-        var txt = document.createTextNode("Click here to comments");
-        href.appendChild(txt);
-        comment.appendChild(href);
+    $.ajax({
+        url:"https://api.github.com/repos/"+_config['owner']+"/"+_config['repo']+"/issues",
+        data:{
+            filter       : 'created',
+            page         : page,
+            // access_token : _config['access_token'],
+            per_page     : _config['per_page']
+        },
+        beforeSend:function(){
+          $('#container').html('<center><img src="loading.gif" class="loading"></center>');
+        },
+        success:function(data, textStatus, jqXHR){
+            var link = jqXHR.getResponseHeader("Link") || "";
+            var next = false;
+            var prev = false;
+            if(link.indexOf('rel="next"') > 0){
+              next = true;
+            }
+            if(link.indexOf('rel="prev"') > 0){
+              prev = true;
+            }
+            var ractive = new Ractive({
+                template : '#listTpl',
+                data     : {
+                    posts : data,
+                    next  : next,
+                    prev  : prev,
+                    page  : page
+                }
+            });
+            window._G.postList[page] = ractive.toHTML();
+            $('#container').html(window._G.postList[page]);
+
+            //将文章列表的信息存到全局变量中，避免重复请求
+            for(i in data){
+              var ractive = new Ractive({
+                  template : '#detailTpl',
+                  data     : {post: data[i]}
+              });
+              window._G.post[data[i].number] = {};
+              window._G.post[data[i].number].body = ractive.toHTML();
+              
+              var title = data[i].title + " | " + _config['blog_name'];
+              window._G.post[data[i].number].title = title;
+            }
+        }
     });
 }
 
+function highlight(){
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+}
+
+// 动态加载多说评论框的函数
+function toggleDuoshuoComments(container, id){
+    var el = document.createElement('div');
+    var url = window.location.href;
+    el.setAttribute('data-thread-key', id);
+    el.setAttribute('data-url', url);
+    DUOSHUO.EmbedThread(el);
+    jQuery(container).append(el);
+}
+
+function detail(id){
+    if(!window._G){
+      window._G = {post: {}, postList: {}};
+      window._G.post[id] = {};  
+    }
+    
+    if(_G.post[id].body != undefined){
+      $('#container').html(_G.post[id].body);
+      $('title').html(_G.post[id].title);
+      toggleDuoshuoComments('#container', id);
+      highlight();
+      return;
+    }
+    $.ajax({
+        url:"https://api.github.com/repos/"+_config['owner']+"/"+_config['repo']+"/issues/" + id,
+        data:{
+            // access_token:_config['access_token']
+        },
+        beforeSend:function(){
+          $('#container').html('<center><img src="loading.gif" alt="loading" class="loading"></center>');
+        },
+        success:function(data){
+            var ractive = new Ractive({
+                 el: "#container",
+                 template: '#detailTpl',
+                 data: {post: data}
+            });
+
+            $('title').html(data.title + " | " + _config['blog_name']);
+            toggleDuoshuoComments('#container', id);
+            highlight();
+        }
+    });  
+
+}
+
+var helpers = Ractive.defaults.data;
+helpers.markdown2HTML = function(content){
+    return marked(content);
+}
+helpers.formatTime = function(time){
+    return time.substr(0,10);
+}
+
+var routes = {
+    '/': index,
+    'p:page': index,
+    'post/:postId': detail
+};
+var router = Router(routes);
+router.init('/');
